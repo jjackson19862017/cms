f9be816d35c4fc573bd5512f3ad81021eb25120f
Stephen Jackson <44205375+jjackson19862017@users.noreply.github.com>
2020-12-05 22:09:52 +0000

    Setup Admin files, links, controllers and components

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index'); //info To make this controller type 'php artisan make:controller AdminsController' in the terminal
