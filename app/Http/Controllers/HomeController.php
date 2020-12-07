b7aac821dfda6c6f11b72dc08e0210d65c8ee7fa
Stephen Jackson <44205375+jjackson19862017@users.noreply.github.com>
2020-12-07 10:57:13 +0000

    Displaying dynamic data on the blog home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,6 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // info To access our posts in the database we will set it into a variable
+        $posts = Post::all();
+        return view('home', ['posts'=>$posts]);
     }
 }
