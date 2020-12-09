e3fd1e1af54c46d560aa840b169f19de788a02e9
Stephen Jackson <44205375+jjackson19862017@users.noreply.github.com>
2020-12-09 14:15:04 +0000

    Created Permissions, Roles, User Tables, and migrated them with 'php artisan migrate'

---
+++ app/Models/Permission.php
@@ -0,0 +1,11 @@
+<?php
+// info Created with 'php artisan make:model Permission -mc'
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/Role.php
@@ -0,0 +1,11 @@
+<?php
+// Info Created with 'php artisan make:model Role -mc'
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+}
