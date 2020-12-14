391a8576cef5284c3c152cd931045c210161dbf4
Stephen Jackson <44205375+jjackson19862017@users.noreply.github.com>
2020-12-14 22:40:05 +0000

    Creating component folders and updating code

--- resources/views/components/admin-master.blade.php
+++ resources/views/components/admin-master.blade.php
@@ -55,11 +55,11 @@
       </div>
 
       <!-- Nav Item - Pages Collapse Menu -->
-      <x-admin-sidebar-posts-links></x-admin-sidebar-posts-links>
+      <x-admin.sidebar.admin-sidebar-posts-links></x-admin.sidebar.admin-sidebar-posts-links>
 
       <!-- Nav Item - Pages Collapse Menu -->
       @if (auth()->user()->userHasRole('Admin'))
-        <x-admin-sidebar-users-links></x-admin-sidebar-users-links>
+        <x-admin.sidebar.admin-sidebar-users-links></x-admin.sidebar.admin-sidebar-users-links>
       @endif
 
       <!-- Nav Item - Utilities Collapse Menu -->
@@ -289,7 +289,7 @@
             <div class="topbar-divider d-none d-sm-block"></div>
 
             <!-- Nav Item - User Information -->
-            <x-admin-top-navbar-user-information></x-admin-top-navbar-user-information>
+            <x-admin.top-nav.admin-top-navbar-user-information></x-admin.top-nav.admin-top-navbar-user-information>
 
           </ul>
 
